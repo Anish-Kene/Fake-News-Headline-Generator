a06d525111c3586689b8b3db66cf6ccc81c6b59b
Anish Kene <anishkene003@gmail.com>
2025-07-12 02:29:43 +0530

    Add files via upload
    Fake News Headline Generator (Python)
    
    This is a fun Python project that generates fake and humorous news headlines by randomly combining words from predefined lists. The project is designed purely for entertainment and demonstrates the use of the random module to create unexpected and funny combinations. It’s a light and simple script that showcases creativity through basic Python programming.

---
+++ interface.py
@@ -0,0 +1,48 @@
+#1- Import the radom module
+import random
+
+#2- Create Subjects
+subjects= [
+    "Shahrukh khan",
+    "Virat Kohli",
+    "Nirmala Sitaraman",
+    "A Mumbaim Cat",
+    "Prime Minister Modi",
+    "Auto Rickshaw Driver From Delhi"
+]
+
+actions = [
+    "Launches",
+    "Cancles",
+    "dance with",
+    "declears war on",
+    "orders",
+    "celebrates"
+]
+
+places_or_things = [
+    "at Red Fort",
+    "in Mumbai Local train",
+    "a plate of Saamosa",
+    "Inside parliament",
+    "at ganga ghat",
+    "During IPL Match",
+    "At India Gate"
+]
+
+#3 start the headline generation loop
+while True:
+    subject = random.choice(subjects)
+    action = random.choice(actions)
+    place_or_thing = random.choice(places_or_things)
+
+    Headline = f" BREAKING NEWS: {subject} {action} {place_or_thing} "
+    print("\n" + Headline)
+
+    user_input = input("\nDo you want another headline ? (yes/no)").strip().lower
+    if user_input == "no":
+        break
+    
+
+#print goodbye message
+print("\n Thanks for using the Fake News Headline Generator. Have a fun day")
